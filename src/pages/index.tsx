7722286f374501e58af7be4138ed9654e1306ed0
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-01-16 15:45:04 -0800

    aaaah

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ import Seo from '../components/Seo';
 // Before you begin editing, follow all comments with `STARTERCONF`,
 // to customize the default configuration.
 
-const socket = io('https://checkkirby.lacontroller.io/');
+const socket = io('https://socket-checkbook-t4gf5mnipq-uw.a.run.app');
 
 interface ButtonToExpandInterface {
   showautocomplete: any;
