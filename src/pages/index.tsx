91bea54f99739248f3784075f6788a89f9a3eede
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-02-12 16:48:51 -0800

    fix bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -124,17 +124,6 @@ export default function HomePage(props: any) {
     });
   }, [initsearchquery]);
 
-  setInterval(() => {
-    if (deptsloaded === false) {
-      socket.emit('mainautocomplete', {
-        querystring: initsearchquery,
-      });
-      socket.emit('fetchdepts', {
-        hi: 'hello',
-      });
-    }
-  }, 300);
-
   useEffect(() => {
     connectToServer();
 
