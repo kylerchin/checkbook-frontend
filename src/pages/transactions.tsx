923a05e5227e8283f4ead2f449a96c1442e086a3
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-02-22 00:09:37 -0800

    bruh

--- src/pages/transactions.tsx
+++ src/pages/transactions.tsx
@@ -5,5 +5,18 @@ import { TransactionTable } from '../components/TransactionTable';
 export default function transactions() {
   //const currentBruh = useRef()
 
-  return <TransactionTable />;
+  return (
+    <TransactionTable
+      optionalcolumns={[
+        'transaction_date',
+        'description',
+        'detailed_item_description',
+        'vendor_name',
+        'department_name',
+        'fund_name',
+        'program_name',
+        'expenditure_type',
+      ]}
+    />
+  );
 }
