121ea2fee9a5e00eb6e6a09237fbbe3a9ccf8d6a
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-02-25 01:35:45 -0800

    add more navbar stuff

--- src/pages/dept/[deptname].tsx
+++ src/pages/dept/[deptname].tsx
@@ -1,5 +1,9 @@
+import Link from 'next/link';
 import * as React from 'react';
 
+import { departmentNameReplace } from '@/components/departmentNameReplace';
+import { Navbar } from '@/components/nav';
+import Seo from '@/components/Seo';
 import { TransactionTable } from '@/components/TransactionTable';
 
 import backends from '@/backends.json';
@@ -9,6 +13,32 @@ export default function Departments(props: any): JSX.Element {
 
   return (
     <>
+      <Navbar themeChanger={props.themeChanger} />
+      <Seo
+        title={`${departmentNameReplace(
+          props.deptname
+        )}|LA Checkbook Department`}
+      />
+      <Link href='/' className='underline'>
+        <p className='underline hover:text-blue-900 dark:text-blue-200 hover:dark:text-blue-50'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='mr-2 inline h-5 w-5'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18'
+            />
+          </svg>
+          Back to Search
+        </p>
+      </Link>
+      <h1>{departmentNameReplace(props.deptname)}</h1>
       <div>
         <div className=''>
           <TransactionTable
