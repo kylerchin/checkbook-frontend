7385106160a2ba8923b0d324136c565dce32edd9
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-02-25 01:37:59 -0800

    add department screen

--- src/pages/dept/[deptname].tsx
+++ src/pages/dept/[deptname].tsx
@@ -14,49 +14,51 @@ export default function Departments(props: any): JSX.Element {
   return (
     <>
       <Navbar themeChanger={props.themeChanger} />
-      <Seo
-        title={`${departmentNameReplace(
-          props.deptname
-        )}|LA Checkbook Department`}
-      />
-      <Link href='/' className='underline'>
-        <p className='underline hover:text-blue-900 dark:text-blue-200 hover:dark:text-blue-50'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            strokeWidth={1.5}
-            stroke='currentColor'
-            className='mr-2 inline h-5 w-5'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18'
+      <div className='mx-auto mx-2 mt-4 dark:text-gray-100'>
+        <Seo
+          title={`${departmentNameReplace(
+            props.deptname
+          )}|LA Checkbook Department`}
+        />
+        <Link href='/' className='underline'>
+          <p className='underline hover:text-blue-900 dark:text-blue-200 hover:dark:text-blue-50'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='mr-2 inline h-5 w-5'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18'
+              />
+            </svg>
+            Back to Search
+          </p>
+        </Link>
+        <h1>{departmentNameReplace(props.deptname)}</h1>
+        <div>
+          <div className=''>
+            <TransactionTable
+              optionalcolumns={[
+                'vendor_name',
+                'fund_name',
+                'program',
+                'expenditure_type',
+                'description',
+                'detailed_item_description',
+              ]}
+              filters={{
+                department: {
+                  query: props.deptname,
+                  matchtype: 'equals',
+                },
+              }}
             />
-          </svg>
-          Back to Search
-        </p>
-      </Link>
-      <h1>{departmentNameReplace(props.deptname)}</h1>
-      <div>
-        <div className=''>
-          <TransactionTable
-            optionalcolumns={[
-              'vendor_name',
-              'fund_name',
-              'program',
-              'expenditure_type',
-              'description',
-              'detailed_item_description',
-            ]}
-            filters={{
-              department: {
-                query: props.deptname,
-                matchtype: 'equals',
-              },
-            }}
-          />
+          </div>
         </div>
       </div>
     </>
