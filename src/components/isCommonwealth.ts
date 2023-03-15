be4df10c30ae606a42a5da4d6bcc28686d319554
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-03-15 16:24:15 -0700

    even more countries

--- src/components/isCommonwealth.ts
+++ src/components/isCommonwealth.ts
@@ -2,6 +2,7 @@ export function isCommonwealth(): boolean {
   if (typeof window !== 'undefined') {
     const browserlanguage = window.navigator.language;
 
+    //add countries that use british english spelling
     const commonwealth = [
       'en-GB',
       'en-AU',
@@ -12,6 +13,20 @@ export function isCommonwealth(): boolean {
       'en-JM',
       'en-BZ',
       'en-TT',
+      'en-ZW',
+      'en-PH',
+      'en-IN',
+      'en-MY',
+      'en-SG',
+      'en-HK',
+      'en-BW',
+      'en-LS',
+      'en-MW',
+      'en-MZ',
+      'en-NA',
+      'en-SZ',
+      'en-UG',
+      'en-ZM',
       //add welsh
       'cy-GB'
     ];
